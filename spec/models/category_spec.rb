067ebcff5d0811ccbd368f1af403794731c079c8
brickstar <brickstar@gmail.com>
2018-05-30 21:49:02 -0600

    wrote test for .ordered

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 describe Category, type: :model do
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
   describe 'relationships' do
     it { should have_many(:gifs) }
+  end
+
+  describe 'instance methods' do
+    describe '.ordered' do
+      category1 = Category.create!(name: 'aardvarks')
+      category4 = Category.create!(name: 'zebras')
+      category2 = Category.create!(name: 'bats')
+      category3 = Category.create!(name: 'vampires')
+
+      expect(Category.ordered).to eq [category1, category2, category3, category4]
+    end
   end
 end
